e523f8e812c8daf56c8ebd881c26770af8d8c27b
Paweł Lewandowski <plewandowski555@gmail.com>
2023-05-30 10:54:15 +0200

    chore(BUX-46): add comment to exported function

--- transports/http/endpoints/api/model.go
+++ transports/http/endpoints/api/model.go
@@ -6,6 +6,7 @@ type ErrorResponse struct {
 	Error string `json:"error"`
 }
 
+// CreateErrorResponse creates ErrorResponse from string.
 func CreateErrorResponse(err string) *ErrorResponse {
 	return &ErrorResponse{
 		Error: err,
