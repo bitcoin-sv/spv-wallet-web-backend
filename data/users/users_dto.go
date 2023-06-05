026e98b66244aa8d11a48447c108c7411f91b9a7
Paweł Lewandowski <plewandowski555@gmail.com>
2023-06-05 12:07:57 +0200

    chore(BUX-48): remove unused method

--- data/users/users_dto.go
+++ data/users/users_dto.go
@@ -14,17 +14,6 @@ type UserDto struct {
 	CreatedAt time.Time `db:"created_at"`
 }
 
-// toUserDto converts User to UserDto.
-func toUserDto(user *users.User) *UserDto {
-	return &UserDto{
-		Id:        user.Id,
-		Email:     user.Email,
-		Xpriv:     user.Xpriv,
-		Paymail:   user.Paymail,
-		CreatedAt: user.CreatedAt,
-	}
-}
-
 // toUser converts UserDto to User.
 func (user *UserDto) toUser() *users.User {
 	return &users.User{
