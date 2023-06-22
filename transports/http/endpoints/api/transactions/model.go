1ddc62669ee453197ab98305a6f83150d19e4f93
Paweł Lewandowski <35259896+pawellewandowski98@users.noreply.github.com>
2023-06-22 10:10:34 +0200

    feat: add CtreateTransaction endpoint (#26)
    * feat: add CtreateTransaction endpoint
    
    * chore: fix typo

--- transports/http/endpoints/api/transactions/model.go
+++ transports/http/endpoints/api/transactions/model.go
@@ -3,6 +3,6 @@ package transactions
 // CreateTransaction represents request for creating new transaction.
 type CreateTransaction struct {
 	Password  string `json:"password"`
-	Recipient string `json:"recipent"`
+	Recipient string `json:"recipient"`
 	Satoshis  uint64 `json:"satoshis"`
 }
