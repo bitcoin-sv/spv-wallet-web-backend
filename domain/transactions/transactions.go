ca24f7e41461d3ff244289ad643192ea1540a2fd
Paweł Lewandowski <plewandowski555@gmail.com>
2023-06-23 16:58:42 +0200

    fix: lint error

--- domain/transactions/transactions.go
+++ domain/transactions/transactions.go
@@ -2,6 +2,8 @@ package transactions
 
 import "bux-wallet/domain/users"
 
+// PaginatedTransactions represents transactions with pagination details
+// like transactins count and number of pages.
 type PaginatedTransactions struct {
 	Count        int64               `json:"count"`
 	Pages        int                 `json:"pages"`
