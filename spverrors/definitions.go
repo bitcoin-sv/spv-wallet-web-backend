cebc658b1583a0bb9541955783aeee738d8f052e
Krzysztof Tomecki <152964795+chris-4chain@users.noreply.github.com>
2024-07-25 11:25:13 +0200

    feat(SPV-915): reformat error definitions

--- spverrors/definitions.go
+++ spverrors/definitions.go
@@ -1,63 +1,197 @@
 package spverrors
 
-import "github.com/bitcoin-sv/spv-wallet/models"
+import (
+	"net/http"
+
+	"github.com/bitcoin-sv/spv-wallet/models"
+)
 
 // ////////////////////////////////// CONTACT ERRORS
 
-var ErrUpsertContact = models.SPVError{Message: "Cannot upsert the contact", StatusCode: 400, Code: "error-upsert-contact"}
+// ErrUpsertContact indicates failure to upsert the contact
+var ErrUpsertContact = models.SPVError{
+	Message:    "Cannot upsert the contact",
+	StatusCode: http.StatusBadRequest,
+	Code:       "error-upsert-contact",
+}
 
-var ErrAcceptContact = models.SPVError{Message: "Cannot accept the contact", StatusCode: 400, Code: "error-accept-contact"}
+// ErrAcceptContact indicates failure to accept the contact
+var ErrAcceptContact = models.SPVError{
+	Message:    "Cannot accept the contact",
+	StatusCode: http.StatusBadRequest,
+	Code:       "error-accept-contact",
+}
 
-var ErrRejectContact = models.SPVError{Message: "Cannot reject the contact", StatusCode: 400, Code: "error-reject-contact"}
+// ErrRejectContact indicates failure to reject the contact
+var ErrRejectContact = models.SPVError{
+	Message:    "Cannot reject the contact",
+	StatusCode: http.StatusBadRequest,
+	Code:       "error-reject-contact",
+}
 
-var ErrConfirmContact = models.SPVError{Message: "Cannot confirm the contact", StatusCode: 400, Code: "error-confirm-contact"}
+// ErrConfirmContact indicates failure to confirm the contact
+var ErrConfirmContact = models.SPVError{
+	Message:    "Cannot confirm the contact",
+	StatusCode: http.StatusBadRequest,
+	Code:       "error-confirm-contact",
+}
 
-var ErrGetContacts = models.SPVError{Message: "Cannot get contacts", StatusCode: 400, Code: "error-get-contacts"}
+// ErrGetContacts indicates failure to get contacts
+var ErrGetContacts = models.SPVError{
+	Message:    "Cannot get contacts",
+	StatusCode: http.StatusBadRequest,
+	Code:       "error-get-contacts",
+}
 
-var ErrGenerateTotpForContact = models.SPVError{Message: "Cannot generate TOTP for contact", StatusCode: 400, Code: "error-generate-totp-for-contact"}
+// ErrGenerateTotpForContact indicates failure to generate TOTP for contact
+var ErrGenerateTotpForContact = models.SPVError{
+	Message:    "Cannot generate TOTP for contact",
+	StatusCode: http.StatusBadRequest,
+	Code:       "error-generate-totp-for-contact",
+}
 
 // ////////////////////////////////// TRANSACTION ERRORS
 
-var ErrCreateTransaction = models.SPVError{Message: "Cannot create transaction", StatusCode: 400, Code: "error-transaction-create"}
+// ErrCreateTransaction indicates failure to create a transaction
+var ErrCreateTransaction = models.SPVError{
+	Message:    "Cannot create transaction",
+	StatusCode: http.StatusBadRequest,
+	Code:       "error-transaction-create",
+}
 
-var ErrGetTransaction = models.SPVError{Message: "Cannot get transaction", StatusCode: 400, Code: "error-transaction-get"}
+// ErrGetTransaction indicates failure to get a transaction
+var ErrGetTransaction = models.SPVError{
+	Message:    "Cannot get transaction",
+	StatusCode: http.StatusBadRequest,
+	Code:       "error-transaction-get",
+}
 
-var ErrGetTransactions = models.SPVError{Message: "Cannot get transactions", StatusCode: 400, Code: "error-transactions-get"}
+// ErrGetTransactions indicates failure to get transactions
+var ErrGetTransactions = models.SPVError{
+	Message:    "Cannot get transactions",
+	StatusCode: http.StatusBadRequest,
+	Code:       "error-transactions-get",
+}
 
-var ErrCountTransactions = models.SPVError{Message: "Cannot count transactions", StatusCode: 400, Code: "error-transactions-count"}
+// ErrCountTransactions indicates failure to count transactions
+var ErrCountTransactions = models.SPVError{
+	Message:    "Cannot count transactions",
+	StatusCode: http.StatusBadRequest,
+	Code:       "error-transactions-count",
+}
 
-var ErrRecordTransaction = models.SPVError{Message: "Cannot record transaction", StatusCode: 400, Code: "error-transaction-record"}
+// ErrRecordTransaction indicates failure to record a transaction
+var ErrRecordTransaction = models.SPVError{
+	Message:    "Cannot record transaction",
+	StatusCode: http.StatusBadRequest,
+	Code:       "error-transaction-record",
+}
 
 // ////////////////////////////////// USER ERRORS
 
-var ErrInvalidCredentials = models.SPVError{Message: "Invalid credentials", StatusCode: 401, Code: "error-credentials-invalid"}
+// ErrInvalidCredentials indicates invalid credentials were provided
+var ErrInvalidCredentials = models.SPVError{
+	Message:    "Invalid credentials",
+	StatusCode: http.StatusUnauthorized,
+	Code:       "error-credentials-invalid",
+}
 
-var ErrUserAlreadyExists = models.SPVError{Message: "User already exists", StatusCode: 409, Code: "error-user-already-exists"}
+// ErrUserAlreadyExists indicates the user already exists
+var ErrUserAlreadyExists = models.SPVError{
+	Message:    "User already exists",
+	StatusCode: http.StatusConflict,
+	Code:       "error-user-already-exists",
+}
 
-var ErrInsertUser = models.SPVError{Message: "Cannot insert new user", StatusCode: 500, Code: "error-user-insert"}
+// ErrInsertUser indicates failure to insert a new user
+var ErrInsertUser = models.SPVError{
+	Message:    "Cannot insert new user",
+	StatusCode: http.StatusInternalServerError,
+	Code:       "error-user-insert",
+}
 
-var ErrEmptyPassword = models.SPVError{Message: "Password cannot be empty", StatusCode: 400, Code: "error-password-empty"}
+// ErrEmptyPassword indicates the password cannot be empty
+var ErrEmptyPassword = models.SPVError{
+	Message:    "Password cannot be empty",
+	StatusCode: http.StatusBadRequest,
+	Code:       "error-password-empty",
+}
 
-var ErrIncorrectEmail = models.SPVError{Message: "Incorrect email", StatusCode: 400, Code: "error-email-incorrect"}
+// ErrIncorrectEmail indicates an incorrect email was provided
+var ErrIncorrectEmail = models.SPVError{
+	Message:    "Incorrect email",
+	StatusCode: http.StatusBadRequest,
+	Code:       "error-email-incorrect",
+}
 
-var ErrRegisterXPub = models.SPVError{Message: "Cannot register new xPub", StatusCode: 400, Code: "error-xpub-register"}
+// ErrRegisterXPub indicates failure to register a new xPub
+var ErrRegisterXPub = models.SPVError{
+	Message:    "Cannot register new xPub",
+	StatusCode: http.StatusBadRequest,
+	Code:       "error-xpub-register",
+}
 
-var ErrRegisterPaymail = models.SPVError{Message: "Cannot register new Paymail", StatusCode: 400, Code: "error-paymail-register"}
+// ErrRegisterPaymail indicates failure to register a new Paymail
+var ErrRegisterPaymail = models.SPVError{
+	Message:    "Cannot register new Paymail",
+	StatusCode: http.StatusBadRequest,
+	Code:       "error-paymail-register",
+}
 
-var ErrGenerateMnemonic = models.SPVError{Message: "Cannot generate mnemonic", StatusCode: 500, Code: "error-mnemonic-generate"}
+// ErrGenerateMnemonic indicates failure to generate a mnemonic
+var ErrGenerateMnemonic = models.SPVError{
+	Message:    "Cannot generate mnemonic",
+	StatusCode: http.StatusInternalServerError,
+	Code:       "error-mnemonic-generate",
+}
 
-var ErrGenerateXPriv = models.SPVError{Message: "Cannot generate xPriv", StatusCode: 500, Code: "error-xpriv-generate"}
+// ErrGenerateXPriv indicates failure to generate an xPriv
+var ErrGenerateXPriv = models.SPVError{
+	Message:    "Cannot generate xPriv",
+	StatusCode: http.StatusInternalServerError,
+	Code:       "error-xpriv-generate",
+}
 
-var ErrEncryptXPriv = models.SPVError{Message: "Cannot encrypt xPriv", StatusCode: 500, Code: "error-xpriv-encrypt"}
+// ErrEncryptXPriv indicates failure to encrypt an xPriv
+var ErrEncryptXPriv = models.SPVError{
+	Message:    "Cannot encrypt xPriv",
+	StatusCode: http.StatusInternalServerError,
+	Code:       "error-xpriv-encrypt",
+}
 
-var ErrDecryptXPriv = models.SPVError{Message: "Cannot decrypt xPriv", StatusCode: 500, Code: "error-xpriv-decrypt"}
+// ErrDecryptXPriv indicates failure to decrypt an xPriv
+var ErrDecryptXPriv = models.SPVError{
+	Message:    "Cannot decrypt xPriv",
+	StatusCode: http.StatusInternalServerError,
+	Code:       "error-xpriv-decrypt",
+}
 
-var ErrGetUser = models.SPVError{Message: "Cannot get user", StatusCode: 500, Code: "error-user-get"}
+// ErrGetUser indicates failure to get user information
+var ErrGetUser = models.SPVError{
+	Message:    "Cannot get user",
+	StatusCode: http.StatusInternalServerError,
+	Code:       "error-user-get",
+}
 
-var ErrCreateAccessKey = models.SPVError{Message: "Cannot create access key", StatusCode: 500, Code: "error-access-key-create"}
+// ErrCreateAccessKey indicates failure to create an access key
+var ErrCreateAccessKey = models.SPVError{
+	Message:    "Cannot create access key",
+	StatusCode: http.StatusInternalServerError,
+	Code:       "error-access-key-create",
+}
 
-var ErrGetXPub = models.SPVError{Message: "Cannot get xPub", StatusCode: 500, Code: "error-xpub-get"}
+// ErrGetXPub indicates failure to get an xPub
+var ErrGetXPub = models.SPVError{
+	Message:    "Cannot get xPub",
+	StatusCode: http.StatusInternalServerError,
+	Code:       "error-xpub-get",
+}
 
-// ////////////////////////////////// RATE
+// ////////////////////////////////// RATE ERRORS
 
-var ErrRateNotFound = models.SPVError{Message: "Rate not found", StatusCode: 404, Code: "error-rate-not-found"}
+// ErrRateNotFound indicates the requested rate was not found
+var ErrRateNotFound = models.SPVError{
+	Message:    "Rate not found",
+	StatusCode: http.StatusNotFound,
+	Code:       "error-rate-not-found",
+}
